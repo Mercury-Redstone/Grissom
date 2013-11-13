80ea7d5bf7f4417a90ac69eff17ac182a7c571da
Martin Sundhaug <martinsundhaug@gmail.com>
2013-11-13 14:18:46 +0100

    Glemte " tegn

--- sql.h
+++ sql.h
@@ -1,9 +1,9 @@
 #ifndef sql_h
 #define sql_h 1
 
-#define SQL_HOST localhost
-#define SQL_USER johdah
-#define SQL_PASS 1234
+#define SQL_HOST "localhost"
+#define SQL_USER "johdah"
+#define SQL_PASS "1234"
 
 #include <mysql/mysql.h>
 
