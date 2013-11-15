53f55f2af0414facbf89c1a8428b4c3a82a6d06a
Martin Sundhaug <martinsundhaug@gmail.com>
2013-11-16 00:35:57 +0100

    ZF for Bruker.sql

--- Bruker.sql
+++ Bruker.sql
@@ -1,12 +1,11 @@
 CREATE TABLE Bruker(
         	bruker_id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-        	fNavn VARCHAR(30) NOT NULL,
-        	eNavn VARCHAR(50) NOT NULL,
+        	navn VARCHAR(90) NOT NULL,
         	epost VARCHAR(50) NOT NULL,
-        	tlf INT(8) NOT NULL,
+        	tlf INT(8) ZEROFILL NOT NULL,
         	adresse VARCHAR(50) NOT NULL,
         	byen VARCHAR(50) NOT NULL,
-        	postkode INT(4) NOT NULL,
+        	postkode INT(4) ZEROFILL NOT NULL,
         	gender ENUM('M','F') NOT NULL DEFAULT 'M',
        	PRIMARY KEY(bruker_id)
 );
