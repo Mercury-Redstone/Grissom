eb6b553eeb97e4295396c57940bd9dba48448977
Martin Sundhaug <martinsundhaug@gmail.com>
2013-11-15 21:10:34 +0100

    Test sql_query

--- main.c
+++ main.c
@@ -6,5 +6,9 @@ int main(int argc, char **argv)
 	printf("sql_init\t");
 	sql_init();
 	printf("Ok\n");
+
+	printf("sql_query\t");
+	MYSQL_RES* result=sql_query("SELECT * FROM Bruker;");
+	printf("Ok\n");
 	return 0;
 }
