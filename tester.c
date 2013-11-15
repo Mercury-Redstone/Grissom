34e9e46445ad249b40acdc8f1bab2a7f789060a0
Martin Sundhaug <martinsundhaug@gmail.com>
2013-11-16 00:35:39 +0100

    Test user adding

--- tester.c
+++ tester.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "sql.h"
+#include "user.h"
 
 int main(int argc, char **argv)
 {
@@ -9,6 +10,11 @@ int main(int argc, char **argv)
 
 	printf("sql_query\t");
 	MYSQL_RES* result=sql_query("SELECT * FROM Bruker;");
+	printf("Ok\n");
+
+	printf("add_user\t");
+	//INSERT INTO Bruker (navn, epost, tlf, adresse, byen, postkode, gender) VALUES (,,,,,,);"
+	add_user("test","test","00000000","test","test","0000",'M');
 	printf("Ok\n");
 	return 0;
 }
