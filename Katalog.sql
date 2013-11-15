568115d14ee615de5e074ba9b7f86c90bcda45ff
Martin Sundhaug <martinsundhaug@gmail.com>
2013-11-15 18:00:25 +0100

    Primary key for katalog

--- Katalog.sql
+++ Katalog.sql
@@ -1,4 +1,5 @@
 CREATE TABLE Katalog(
     katalog_id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, 
-    navn VARCHAR(50) NOT NULL 
+    navn VARCHAR(50) NOT NULL,
+    PRIMARY KEY(katalog_id)
     ); 
