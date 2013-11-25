f7723859f5ad0dc4a31d3cfa6c0a3312626e5564
Martin Sundhaug <martinsundhaug@gmail.com>
2013-11-25 20:14:42 +0100

    Implemented prevHashFor and fileChanged

--- hash.h
+++ hash.h
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include "sql.h"
+#include "file.h"
 
 char* hash(char* fileName) {
 	char* cmd=malloc(strlen("sha512sum ")+strlen(fileName));
@@ -8,4 +10,18 @@ char* hash(char* fileName) {
 	fscanf(file, "%128s", ret);
 //	printf(ret);
 	return ret;
+}
+
+char* prevHashFor(char* fileName) {
+	char* buf=malloc(128);
+	char* fileId=getFileId(fileName);
+	if(strcmp(fileId,"")) {
+		sprintf(buf,"SELECT hash FROM Endring WHERE fil_id=%s", fileId);
+		sql_query(buf);
+	}
+	return "";
+}
+
+int fileChanged(char* fileName) {
+	return strcmp(prevHashFor(fileName), hash(fileName));
 }
