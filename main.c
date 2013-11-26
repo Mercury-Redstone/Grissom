b583853288823692a78bdc593b6609759e7712f1
Martin Sundhaug <martinsundhaug@gmail.com>
2013-11-26 21:55:27 +0100

    Minor fixes to main.c

--- main.c
+++ main.c
@@ -1,25 +1,24 @@
-#include "fil.h" 
+#include "file.h" 
 #include <stdio.h> 
 #include <string.h> 
 
-int main(void) { 
-	
-	char *inp[]; 
-	char temp[100]; 
-	int i = 0; 
-	static const char *Nameholder[] = {"Navn på fil:", "Id til bruker:", "Id til gruppe", "Id til katalog:", "Id til eier:", "Skal filen overvåkes, 0 for nei og 1 for ja"); 
-	while (i)
-		{
-		printf("%s", Nameholder[i]); 
-		gets(temp); 
-		inp[i] = malloc(strlen(temp)); 
-		inp[i] = temp; 
-		
-	if (i > 5)
-		break; 
-	i ++; 
+int main(void) {
+	char *inp[5];
+	char temp[100];
+	static const char *Nameholder[] = {"Navn på fil:", "Id til bruker:", "Id til gruppe", "Id til katalog:", "Id til eier:", "Skal filen overvåkes, 0 for ja"};
+
+	for(;;) {
+		int i = 0;
+		while (i) {
+			printf("%s", Nameholder[i]);
+			gets(temp);
+			inp[i] = malloc(strlen(temp));
+			inp[i] = temp;
+
+			if (i > 5)
+				break;
+			i++;
+		}
+		add_file(inp[0], atoi(inp[1]), atoi(inp[2]), atoi(inp[3]), atoi(inp[4]), atoi(inp[5]));
 	}
-	
-	add_file(inp[0], inp[1], inp[2], inp[3], inp[4], inp[5]); 
-	
-}	
+}
