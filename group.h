d579365b0b9740b4e061aa3e449ea66cd790ec37
Martin Sundhaug <martinsundhaug@gmail.com>
2013-11-26 22:11:52 +0100

    Implemented getUid and getGid

--- group.h
+++ group.h
@@ -12,6 +12,9 @@ char* getGid(char*);
 #define SQL_INSERT_GROUP_BASE	"INSERT INTO Gruppe (gruppe_Navn) VALUES ('');"
 #define SQL_INSERT_GROUP_FORMAT	"INSERT INTO Gruppe (gruppe_Navn) VALUES ('%s');"
 
+#define SQL_SELECT_GROUP_ID_BASE      "SELECT gruppe_id FROM Gruppe WHERE gruppe_Navn=''"
+#define SQL_SELECT_GROUP_ID_FORMAT    "SELECT gruppe_id FROM Gruppe WHERE gruppe_Navn='%s'"
+
 void add_group(char* navn) {
 //	if(mysql==NULL) sql_init();
 	char* buf=malloc(strlen(SQL_INSERT_GROUP_BASE)+strlen(navn)+1);
@@ -21,5 +24,12 @@ void add_group(char* navn) {
 }
 
 char* getGid(char* navn) {
+	char* buf=malloc(strlen(SQL_SELECT_GROUP_ID_BASE)+strlen(navn)+1);
+        sprintf(buf, SQL_SELECT_GROUP_ID_FORMAT, navn);
+        SQL_RES* res=sql_query(buf);
+        SQL_ROW row=sql_fetch_row(res);
+        char* buf2=row[0];
+        sql_free_result(res);
+        return buf2;
 }
 #endif
--- user.h
+++ user.h
@@ -7,6 +7,9 @@
 #define SQL_INSERT_USER_BASE	"INSERT INTO Bruker (navn, epost, tlf, adresse, byen, postkode, gender) VALUES ('','','','','','','');"
 #define SQL_INSERT_USER_FORMAT	"INSERT INTO Bruker (navn, epost, tlf, adresse, byen, postkode, gender) VALUES ('%s','%s','%s','%s','%s','%s','%c');"
 
+#define SQL_SELECT_USER_ID_BASE      "SELECT bruker_id FROM Katalog WHERE navn=''"
+#define SQL_SELECT_USER_ID_FORMAT    "SELECT bruker_id FROM Katalog WHERE navn='%s'"
+
 void add_user(char* navn, char* epost, char* tlf, char* adresse, char* byen, char* postkode, char gender) {
 	if(mysql==NULL) sql_init();
 	char* buf=malloc(strlen(SQL_INSERT_USER_BASE)+strlen(navn)+strlen(epost)+strlen(tlf)+strlen(adresse)+strlen(byen)+strlen(postkode)+1+1);
@@ -15,6 +18,13 @@ void add_user(char* navn, char* epost, char* tlf, char* adresse, char* byen, cha
 	free(buf);
 }
 char* getUid(char* navn) {
+        char* buf=malloc(strlen(SQL_SELECT_USER_ID_BASE)+strlen(navn)+1);
+        sprintf(buf, SQL_SELECT_USER_ID_FORMAT, navn);
+        SQL_RES* res=sql_query(buf);
+        SQL_ROW row=sql_fetch_row(res);
+        char* buf2=row[0];
+        sql_free_result(res);
+        return buf2;
 }
 
 #endif
