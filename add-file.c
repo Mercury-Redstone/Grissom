1b1e194fafcca79e39ee8ac00ea0522f78e9d438
Martin Sundhaug <martinsundhaug@gmail.com>
2013-11-26 22:06:45 +0100

    Changed some function signatures

--- add-file.c
+++ add-file.c
@@ -2,5 +2,5 @@
 int main(int argc, char** argv) {
 	assert(argc==7);
 //	sql_init();
-	add_file(argv[1],atoi(argv[2]),atoi(argv[3]),atoi(argv[4]),atoi(argv[5]),atoi(argv[6]));
+	add_file(argv[1],argv[2],argv[3],argv[4],argv[5],atoi(argv[6]));
 }
--- file.h
+++ file.h
@@ -10,7 +10,7 @@
 
 #define SQL_SELECT_FILE_ID_BASE "SELECT fil_id FROM Fil WHERE filNavn='' AND katalog_id=''"
 #define SQL_SELECT_FILE_ID_FORMAT "SELECT fil_id FROM Fil WHERE filNavn='%s' AND katalog_id='%s'"
-void add_file(char* navn, int bruker, int gruppe, int katalog, int eier, int skal_overvake) {
+void add_file(char* navn, char* bruker, char* gruppe, char* katalog, char* eier, int skal_overvake) {
 //	if(mysql==NULL) sql_init();
 	char* buf=malloc(strlen(SQL_INSERT_FILE_BASE)+strlen(navn)+4*5*sizeof(int));
 	sprintf(buf, SQL_INSERT_FILE_FORMAT, navn, bruker, gruppe, katalog, eier, skal_overvake);
