f1d62e8e1fd55dcdde88779fd369ded545dbb244
Martin Sundhaug <martinsundhaug@gmail.com>
2013-11-27 13:56:59 +0100

    Minor changes to revision

--- add-revision.c
+++ add-revision.c
@@ -2,5 +2,5 @@
 
 int main(int argc, char** argv) {
 //	sql_init();
-	add_revision(argv[1],argv[2],argv[3],argv[4]);
+	add_revision(argv[1],argv[2],argv[3],atoi(argv[4]));
 }
--- revision.h
+++ revision.h
@@ -7,9 +7,9 @@
 #define SQL_INSERT_REVISION_BASE	"INSERT INTO Endring (fil_id, hash, dato, generasjon) VALUES ('','','','');"
 #define SQL_INSERT_REVISION_FORMAT	"INSERT INTO Endring (fil_id, hash, dato, generasjon) VALUES ('%s','%s','%s','%d');"
 
-void add_revision(char* fil, char* hash, char* dato, char* generasjon) {
+void add_revision(char* fil, char* hash, char* dato, int generasjon) {
 //	if(mysql==NULL) sql_init();
-	char* buf=malloc(strlen(SQL_INSERT_REVISION_BASE)+strlen(fil)+strlen(hash)+strlen(dato)+strlen(generasjon)+1);
+	char* buf=malloc(strlen(SQL_INSERT_REVISION_BASE)+strlen(fil)+strlen(hash)+strlen(dato)+8+1);
 	sprintf(buf, SQL_INSERT_REVISION_FORMAT, fil,hash,dato,generasjon);
 	sql_query(buf);
 	free(buf);
