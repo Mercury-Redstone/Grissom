9221d12ed1ed5d09b36ec2e6a4c9e789a1bf7221
Martin Sundhaug <martinsundhaug@gmail.com>
2013-11-27 13:57:53 +0100

    Implemented checkableFiles

--- file.h
+++ file.h
@@ -10,6 +10,8 @@
 
 #define SQL_SELECT_FILE_ID_BASE "SELECT fil_id FROM Fil WHERE filNavn='' AND katalog_id=''"
 #define SQL_SELECT_FILE_ID_FORMAT "SELECT fil_id FROM Fil WHERE filNavn='%s' AND katalog_id='%s'"
+
+#define SQL_SELECT_FILE_ID_CHECKABLE   "SELECT fil_id FROM Fil WHERE skal_overvake='1'"
 void add_file(char* navn, char* bruker, char* gruppe, char* katalog, char* eier, int skal_overvake) {
 //	if(mysql==NULL) sql_init();
 	char* buf=malloc(strlen(SQL_INSERT_FILE_BASE)+strlen(navn)+4*5*sizeof(int));
@@ -35,6 +37,20 @@ char* getFileId(char* navn) {
 	int rows=sql_num_rows(res);
 	if(rows==1) {ret=sql_fetch_row(res)[0];sql_free_result(res);}
 	else ret="";
+	return ret;
+}
+
+char** checkableFiles() {
+	SQL_RES* res=sql_query(SQL_SELECT_FILE_ID_CHECKABLE);
+	char** ret=(char**)malloc((sql_num_rows(res)+1)*sizeof(char*));
+	int i=0;
+	char** row=sql_fetch_row(res);
+	while(i<sql_num_rows(res)) {
+		ret[i]=strdup(row[i]);
+		i++;
+	}
+	ret[i]=NULL;
+	sql_free_result(res);
 	return ret;
 }
 #endif
