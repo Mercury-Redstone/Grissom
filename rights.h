e053d35f8beffd325ba9fd224b1a92ceead1419d
Martin Sundhaug <martinsundhaug@gmail.com>
2013-11-27 16:51:45 +0100

    minor additions

--- rights.h
+++ rights.h
@@ -1,13 +1,12 @@
-#ifndef rettighet_h
-#define rettighet_h
+#ifndef rights_h
+#define rights_h
 
 #include "sql.h"
 #include <string.h>
 
-#define SQL_SELECT_FILE_ID_BASE "SELECT liste_id FROM ListeRettigheter"
-#define SQL_SELECT_FILE_ID_FORMAT "SELECT liste_id FROM ListeRettigheter"
+#define SQL_SELECT_RIGHTS_ID_BASE "SELECT liste_id FROM ListeRettigheter"
+#define SQL_SELECT_RIGHTS_ID_FORMAT "SELECT liste_id FROM ListeRettigheter"
 
 const string[3] = "Re01"
 
-
-
+#endif
